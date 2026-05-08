e64db5500d855537da979ed1c76e72dd886f9381
Isha Bhelsekar <113613452+i-s-h-a-b@users.noreply.github.com>
2026-05-08 23:43:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 30) {
+        navbar.style.boxShadow = '0 2px 10px rgba(0,0,0,0.08)';
+    } else {
+        navbar.style.boxShadow = 'none';
+    }
+});
